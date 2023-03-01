bda5f3a89adfa960a8191ae2858a991c2ee10965
Kieran Tahir <xerooutreach@Kieran.local>
2023-03-01 13:44:38 +1300

    variables updated

--- variables.js
+++ variables.js
@@ -1,14 +1,22 @@
+//------------------------------------------------------------ VAR ------------------------------------------------------------
+
 var dolphin
 var dolphin = 'nice'
 dolphin = 'cool'
 
 console.log('var dolphin =', dolphin)
 
+
+//------------------------------------------------------------ LET ------------------------------------------------------------
+
 let whale
 // let whale = 'fun' // error because 'let' can only be declared once
 whale = 'awesome'
 
 console.log('let whale =', whale)
+
+
+//------------------------------------------------------------ CONST ------------------------------------------------------------
 
 // const shark // error because 'const' must be declared with a value
 const shark = 'scary'
