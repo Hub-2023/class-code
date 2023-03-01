69be975b3cc88f96fe2bf017ac99f47bc34ad8e5
Kieran Tahir <xerooutreach@Kieran.local>
2023-03-01 13:37:00 +1300

    variables updated

---
+++ variables.js
@@ -0,0 +1,17 @@
+var dolphin
+var dolphin = 'nice'
+dolphin = 'cool'
+
+console.log('var dolphin =', dolphin)
+
+let whale
+// let whale = 'fun' // error because 'let' can only be declared once
+whale = 'awesome'
+
+console.log('let whale =', whale)
+
+// const shark // error because 'const' must be declared with a value
+const shark = 'scary'
+// shark = 'dangerous' // error because 'const' cannot be reassigned
+
+console.log('const shark =', shark)
