52e853b52c46061f6d97c88a8264882fc57185c2
Kieran <kierantahir@gmail.com>
2023-03-17 10:01:29 +1300

    lunch.js update

--- sprint2/lunch.js
+++ sprint2/lunch.js
@@ -1,11 +1,15 @@
 const meals = ['subway', 'sushi', 'kebab']
 
+// the line below needs to take meals as a parameter:
 function lunch (meals){
+    // the line below needs to be a 'conditional' statement (if the meal is subway, eat fresh, otherwise don't eat fresh):
     if (meals[0] === 'subway'){
         console.log("eat fresh")
     } else {
+        // the line below needs double quotes or an escape character:
         console.log("don't eat fresh")
     }
 }
 
-lunch(meals[2])
+//the line below needs to call the lunch function, with the meals array as an argument:
+lunch(meals[0])
